a367e24bb2d46d2403e2c68a1ecc2211dc68868c
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-08-20 02:18:03 +0800

    feat: add info message for scripts

--- run.py
+++ run.py
@@ -2,10 +2,19 @@ import schedule
 import time
 import subprocess
 
+
 def run_script():
     print("Time to get AI data!")
     try:
-        result = subprocess.run(['/GPFS/rhome/xiyuanyang/Agents/AutoEmailSender/.venv/bin/python', 'main.py'], check=True, capture_output=True, text=True)
+        result = subprocess.run(
+            [
+                "/GPFS/rhome/xiyuanyang/Agents/AutoEmailSender/.venv/bin/python",
+                "main.py",
+            ],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
         print("Success!")
         print(result.stdout)
     except subprocess.CalledProcessError as e:
@@ -14,8 +23,13 @@ def run_script():
     except Exception as e:
         print(f"Error: {e}")
 
+
 if __name__ == "__main__":
     schedule.every().day.at("23:00").do(run_script)
+    count = 0
     while True:
         schedule.run_pending()
         time.sleep(5)
+        if count == 0:
+            print("I am not dead...")
+        count = (count + 1) % 10000
